5ac4b5ea078df125c718e36b90b7d10b58718c04
Jenna Jones <jljones121@gmail.com>
2024-07-16 23:52:29 -0500

    task 0 and 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,54 +1,31 @@
-#!/usr/bin/python3
-"""Exports to-do list information for a given user ID to CSV format."""
+i#!/usr/bin/python3
+"""Exports to-do list info for a given employee ID to CSV format.
+"""
 
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    # Check if user ID is provided as a command-line argument
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <user_id>")
-        sys.exit(1)
-
     user_id = sys.argv[1]
 
     # Define the base URL for the JSON API
-    base_url = "https://jsonplaceholder.typicode.com/"
+    url = "https://jsonplaceholder.typicode.com/"
 
-    try:
-        # Fetch user information from the API
-        user_response = requests.get(f"{base_url}users/{user_id}")
-        user_response.raise_for_status()
-        user = user_response.json()
-        username = user.get("username")
+    # Fetch user information from the API and
+    #   convert the response to a JSON object
+    user = requests.get(url + "users/{}".format(user_id)).json()
 
-        # Fetch the to-do list items associated with the user ID
-        todos_response = requests.get(
-            f"{base_url}todos",
-            params={"userId": user_id}
-        )
-        todos_response.raise_for_status()
-        todos = todos_response.json()
+    # Extract the username from the user data
+    username = user.get("username")
 
-        # Write to CSV file
-        with open(f"{user_id}.csv", "w", newline="",
-                  encoding="utf-8") as csvfile:
-            csv_writer = csv.writer(csvfile)
-            csv_writer.writerow(["User ID", "Username", "Completed", "Title"])
+    todos = requests.get(
+        url + "todos",
+        params={"userId": user_id}
+    ).json()
 
-            for todo in todos:
-                csv_writer.writerow(
-                    [user_id, username, todo["completed"], todo["title"]])
-
-        print(f"CSV file '{user_id}.csv' has been successfully created.")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
-        sys.exit(1)
-
-    except (KeyError, IndexError) as e:
-        print(
-            f"Error: {e}. Please check if the provided
-            user ID '{user_id}' is valid.")
-        sys.exit(1)
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
